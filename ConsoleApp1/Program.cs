79744c6e0d467e216f519a8002a4684be5955b2f
SashaYaroshenko <yaroshenko2019ol@gmail.com>
2022-10-11 21:34:30 +0300

    Add new files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DigPow.digPow(-89, 1);
+        }
+    }
+
+    public class DigPow
+    {
+        public static long digPow(int n, int p)
+        {        
+            n = Math.Abs(n);
+            char[]chars = n.ToString().ToCharArray();
+            List<double> charsDouble = new List<double>();
+            for(int i = 0; i < chars.Length; i++)
+            {
+                charsDouble.Add(Convert.ToDouble(chars[i].ToString()));
+            }
+            double sum = 0;
+            for(int i = 0; i < charsDouble.Count; i++)
+            {
+                sum += Math.Pow(charsDouble[i], p);
+                p++;
+            }
+            if(sum<n)
+            {
+                return -1;
+            }
+            double res = sum/ n;
+            return Convert.ToInt64(res.ToString());
+        }
+    }
+}
---
+++ playing with digits/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using ConsoleApp1;
+
+[TestFixture]
+public class DigPowTests
+{
+
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual(1, DigPow.digPow(89, 1));
+    }
+    [Test]
+    public void Test2()
+    {
+        Assert.AreEqual(-1, DigPow.digPow(92, 1));
+    }
+    [Test]
+    public void Test3()
+    {
+        Assert.AreEqual(51, DigPow.digPow(46288, 3));
+    }
+}
